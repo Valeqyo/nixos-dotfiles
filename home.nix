6685bf4e27477ef9e6e6542ff9b5a81d531d2910
Valeqyo <comariusdorel@gmail.com>
2026-07-08 09:46:25 +0200

    alias update aggiunto + prova

--- home.nix
+++ home.nix
@@ -20,6 +20,7 @@ in
     shellAliases = {
       btw = "echo I use nixos, btw";
       up = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos-btw";
+      update = "~/nixos-dotfiles/scripts/update";
     };
   };
 
