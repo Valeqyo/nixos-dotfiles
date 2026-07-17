50534f2d2342c8758f19b9fe1bd05c74ddc4bfe1
Valeqyo <comariusdorel@gmail.com>
2026-07-17 18:13:38 +0200

    grub

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,9 +1,19 @@
 { ... }:
 
 {
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.loader.systemd-boot.configurationLimit = 20;
+#   # Use the systemd-boot EFI boot loader.
+#   boot.loader.systemd-boot.enable = true;
+#   boot.loader.efi.canTouchEfiVariables = true;
+# 
+#   boot.loader.systemd-boot.configurationLimit = 20;
+  boot.loader = {
+    grub = {
+      enable = true;
+      device = "nodev"; # "nodev" is used for UEFI
+      efiSupport = true;
+      useOSProber = true;
+      configurationLimit = 20;
+    };
+    efi.canTouchEfiVariables = true;
+  };
 }
