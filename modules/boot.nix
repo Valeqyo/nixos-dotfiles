d5cc2bf174cc50fc25f2a5380c1c1b0b3e22d3c5
Valeqyo <comariusdorel@gmail.com>
2026-07-19 10:51:54 +0200

    limine

--- modules/boot.nix
+++ modules/boot.nix
@@ -6,13 +6,40 @@
 #   boot.loader.efi.canTouchEfiVariables = true;
 # 
 #   boot.loader.systemd-boot.configurationLimit = 20;
+
+  # boot.loader = {
+  #   grub = {
+  #     enable = true;
+  #     device = "nodev"; # "nodev" is used for UEFI
+  #     efiSupport = true;
+  #     useOSProber = true;
+  #     configurationLimit = 20;
+  #   };
+  #   efi.canTouchEfiVariables = true;
+  # };
+
   boot.loader = {
-    grub = {
+    limine = {
       enable = true;
-      device = "nodev"; # "nodev" is used for UEFI
-      efiSupport = true;
-      useOSProber = true;
-      configurationLimit = 20;
+      maxGenerations = 20;
+      enableEditor = false;
+      resolution = "1920x1080";
+      style = {
+        wallpapers = [ ../assets/Lowpoly_Street.png ];
+        wallpaperStyle = "stretched";
+        interface.brandingColor = "c4a7e7";
+      };
+      extraEntries = ''
+        /Windows
+        comment: Boot into Windows
+        protocol: efi
+        path: guid(8166987e-4647-4b7b-b22c-6e03dc8d1051):/EFI/Microsoft/Boot/bootmgfw.efi
+      
+        /openSUSE
+        comment: Boot into openSUSE (temporaneo, da rimuovere quando la elimini)
+        protocol: efi
+        path: guid(8166987e-4647-4b7b-b22c-6e03dc8d1051):/EFI/systemd/systemd-bootx64.efi
+      '';
     };
     efi.canTouchEfiVariables = true;
   };
